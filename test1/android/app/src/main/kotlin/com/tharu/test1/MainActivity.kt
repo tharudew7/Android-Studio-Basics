a45987d798891f2bd6cf350876f052f19df69dd7
tharudew7 <73303151+tharudew7@users.noreply.github.com>
2021-03-16 01:01:11 +0530

    test01

---
+++ test1/android/app/src/main/kotlin/com/tharu/test1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tharu.test1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Text("Hello World!"),
+
+    );
+  }
+
+
+
+
+}
+
+
